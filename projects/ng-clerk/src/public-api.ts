a781426186cc63bc955b7d05f585287c9099621b
Thomas Clague <thomasclague19@gmail.com>
2024-05-08 02:24:56 +0100

    fix: exposing components and service

--- projects/ng-clerk/src/public-api.ts
+++ projects/ng-clerk/src/public-api.ts
@@ -1,4 +1,15 @@
 /*
  * Public API Surface of ng-clerk
  */
-export * from './lib/sample.component';
+
+export * from './lib/components/create-organisation.component';
+export * from './lib/components/organisation-list.component';
+export * from './lib/components/organisation-profile.component';
+export * from './lib/components/organisation-switcher.component';
+export * from './lib/components/sign-in.component';
+export * from './lib/components/sign-up.component';
+export * from './lib/components/user-button.component';
+export * from './lib/components/user-profile.component';
+export * from './lib/initializer';
+export * from './lib/injectors/injection-token';
+export * from './lib/services/clerk.service';
